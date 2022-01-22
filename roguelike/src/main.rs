a41c5318966f1de356d0973e7daa1f82c53623b1
Justin's Home PC <jalanmiddleton@gmail.com>
2022-01-21 19:16:44 -0500

    Storing the map.

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -11,6 +11,34 @@ mod prelude {
 
 use prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
 }

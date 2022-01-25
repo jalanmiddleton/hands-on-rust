66d73067cb462900cf81705fa7707d125200421e
Justin's Home PC <jalanmiddleton@gmail.com>
2022-01-24 20:17:14 -0500

    Waiting -> recovers health

--- roguelike/src/systems/player_input.rs
+++ roguelike/src/systems/player_input.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -24,9 +26,9 @@ pub fn player_input(
             .iter(ecs)
             .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
             .unwrap();
-        *turn_state = TurnState::PlayerTurn;
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -34,6 +36,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -44,6 +47,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -53,5 +57,16 @@ pub fn player_input(
                 ));
             }
         }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
+            }
+        }
+        *turn_state = TurnState::PlayerTurn;
     }
 }

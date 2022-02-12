d97ed86b96cd6f030f458b7e9c3207fb6d967d17
Justin's Home PC <jalanmiddleton@gmail.com>
2022-02-11 22:46:28 -0500

    Cellular automata map generation.

--- roguelike/src/map_builder/automata.rs
+++ roguelike/src/map_builder/automata.rs
@@ -1,26 +1,33 @@
-use crate::prelude::*;
 use super::MapArchitect;
+use crate::prelude::*;
 
 pub struct CellularAutomataArchitect {}
 
 impl MapArchitect for CellularAutomataArchitect {
-    fn new(&self, rng: &mut RandomNumberGenerator) -> {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
         let mut mb = MapBuilder {
             map: Map::new(),
             rooms: Vec::new(),
             monster_spawns: Vec::new(),
             player_start: Point::zero(),
-            amulet_start: Point::zero()
+            amulet_start: Point::zero(),
         };
 
+        self.random_noise_map(rng, &mut mb.map);
+        for _ in 0..10 {
+            self.iteration(&mut mb.map);
+        }
+
+        let start = self.find_start(&mb.map);
+        mb.monster_spawns = mb.spawn_monsters(&start, rng);
+        mb.player_start = start;
+        mb.amulet_start = mb.find_most_distant();
         mb
     }
+}
 
-    fn random_noise_map(
-        &mut self,
-        rng: &mut RandomNumberGenerator,
-        map: &mut Map
-    ) {
+impl CellularAutomataArchitect {
+    fn random_noise_map(&mut self, rng: &mut RandomNumberGenerator, map: &mut Map) {
         map.tiles.iter_mut().for_each(|t| {
             let roll = rng.range(0, 100);
             if roll > 55 {
@@ -35,7 +42,7 @@ impl MapArchitect for CellularAutomataArchitect {
         let mut neighbors = 0;
         for iy in -1..=1 {
             for ix in -1..=1 {
-                if !(ix==0 && iy==0) && map.tiles[map_idx(x+ix, y+iy)] == TileType::Wall {
+                if !(ix == 0 && iy == 0) && map.tiles[map_idx(x + ix, y + iy)] == TileType::Wall {
                     neighbors += 1;
                 }
             }
@@ -46,8 +53,8 @@ impl MapArchitect for CellularAutomataArchitect {
 
     fn iteration(&mut self, map: &mut Map) {
         let mut new_tiles = map.tiles.clone();
-        for y in 1..SCREEN_HEIGHT-1 {
-            for x in 1..SCREEN_WIDTH-1 {
+        for y in 1..SCREEN_HEIGHT - 1 {
+            for x in 1..SCREEN_WIDTH - 1 {
                 let neighbors = self.count_neighbors(x, y, map);
                 let idx = map_idx(x, y);
                 if neighbors > 4 || neighbors == 0 {
@@ -62,18 +69,19 @@ impl MapArchitect for CellularAutomataArchitect {
     }
 
     fn find_start(&self, map: &Map) -> Point {
-        let center = Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
-        let closest_point = map.tiles 
+        let center = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+        let closest_point = map
+            .tiles
             .iter()
             .enumerate()
             .filter(|(_, t)| **t == TileType::Floor)
-            .map(|(idx, _)| (idx, DistanceAlg::Pythagoras.distance2d(
-                center,
-                map.index_to_point2d(idx)
-            )))
-            .min_by(|(_, distance), (_, distance2)|
-                distance.partial_cmp(&distance2).unwrap()
-            )
+            .map(|(idx, _)| {
+                (
+                    idx,
+                    DistanceAlg::Pythagoras.distance2d(center, map.index_to_point2d(idx)),
+                )
+            })
+            .min_by(|(_, distance), (_, distance2)| distance.partial_cmp(&distance2).unwrap())
             .map(|(idx, _)| idx)
             .unwrap();
         map.index_to_point2d(closest_point)
